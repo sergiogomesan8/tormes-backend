2d098f84de2b5e547ed8d418632300dab7db5bfa
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-02 21:19:52 +0100

    update app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,4 @@
 import { Controller } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  /*  @Get()
-  getHello(@Request() req) {
-    const user: UserDto = req.user;
-    if (user.id === null) {
-      throw new BadRequestException(
-        'Bad request. Invalid data provided: userId is null',
-      );
-    }
-    return user;
-  }*/
-}
+export class AppController {}
