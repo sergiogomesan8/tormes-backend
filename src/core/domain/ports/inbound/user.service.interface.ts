7f3cbc3dadd4310ab63e7132acf5cd9d193bea1f
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-02 20:15:04 +0100

    add user service

--- src/core/domain/ports/inbound/user.service.interface.ts
+++ src/core/domain/ports/inbound/user.service.interface.ts
@@ -1,5 +1,11 @@
-import { UserPassDto } from 'src/infraestructure/api-rest/dtos/user.dto';
+import { UserEntity } from 'src/infraestructure/postgres/entities/user.entity';
+import { User } from '../../models/user.model';
+import { CreateUserDto } from 'src/infraestructure/api-rest/dtos/user.dto';
 
 export interface IUserService {
-  createUser(user: UserPassDto);
+  create(user: CreateUserDto): Promise<UserEntity | null>;
+  update(user: User): User;
+  delete(id: string): User;
+  findOneBy(id: string): Promise<UserEntity | null>;
+  findAll(): User[];
 }
--- src/core/domain/services/user.service.ts
+++ src/core/domain/services/user.service.ts
@@ -1,32 +1,45 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { IUserService } from '../ports/inbound/user.service.interface';
-import { UserRepository } from '../ports/outbound/user.repository.interface';
-import { User } from '../models/user';
-import {
-  UserDto,
-  UserPassDto,
-} from 'src/infraestructure/api-rest/dtos/user.dto';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/infraestructure/postgres/entities/user.entity';
+import { User } from '../models/user.model';
+import { CreateUserDto } from 'src/infraestructure/api-rest/dtos/user.dto';
 
 @Injectable()
 export class UserService implements IUserService {
   constructor(
-    @Inject(UserRepository)
-    private readonly userPersistence: UserRepository,
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
   ) {}
 
-  async createUser(userPassDto: UserPassDto): Promise<UserDto> {
-    const newUser: User = {
-      id: '',
-      email: userPassDto.email,
-      name: {
-        name: userPassDto.name.name,
-        lastName: userPassDto.name.lastName,
-      },
-      gender: userPassDto.gender,
-      birthdate: userPassDto.birthdate,
-    };
+  async create(createUserDto: CreateUserDto): Promise<UserEntity | null> {
+    try {
+      const user = this.userRepository.create(createUserDto);
+      await this.userRepository.save(user);
 
-    const result = await this.userPersistence.createUser(newUser);
-    return result as UserDto;
+      return user;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  update(user: User): User {
+    console.log(user);
+    throw new Error('Method not implemented.');
+  }
+
+  delete(id: string): User {
+    console.log(id);
+    throw new Error('Method not implemented.');
+  }
+
+  findOneBy(id: string): Promise<UserEntity | null> {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  findAll(): User[] {
+    throw new Error('Method not implemented.');
   }
 }
