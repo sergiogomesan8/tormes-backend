c126854242449b55dbd96d9c1374ead032b75688
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-02 20:14:22 +0100

    create user entity

---
+++ src/infraestructure/postgres/entities/user.entity.ts
@@ -0,0 +1,83 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+export enum UserType {
+  customer = 1,
+  employee = 2,
+  manager = 3,
+}
+
+export enum NotificationPreference {
+  email = 1,
+  sms = 2,
+}
+
+export enum Gender {
+  man = 1,
+  women = 2,
+  other = 3,
+}
+
+@Entity({ name: 'users' })
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column({ nullable: false })
+  lastName: string;
+
+  @Column({ nullable: true })
+  phoneNumber?: number;
+
+  @Column({
+    type: 'enum',
+    enum: NotificationPreference,
+    nullable: true,
+  })
+  notificationPreference?: NotificationPreference;
+
+  @Column({ default: false })
+  verifiedEmail?: boolean;
+
+  @Column({ default: false })
+  verifiedPhoneNumber?: boolean;
+
+  @Column({ nullable: true })
+  deliveryAddress?: string;
+
+  @Column({ nullable: true })
+  billingAddress?: string;
+
+  @CreateDateColumn()
+  createdAt?: number;
+
+  @Column({ nullable: true })
+  lastLogin?: number;
+
+  @Column({ nullable: true })
+  birthdate?: number;
+
+  @Column({
+    type: 'enum',
+    enum: Gender,
+    nullable: true,
+  })
+  gender?: Gender;
+
+  @Column({
+    type: 'enum',
+    enum: UserType,
+    nullable: true,
+  })
+  userType?: UserType;
+}
