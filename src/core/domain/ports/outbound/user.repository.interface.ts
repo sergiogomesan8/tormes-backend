bc13044d4a71386f894b372c7d44ab25bf1b7b6c
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-02 20:16:53 +0100

    remove userRepository

--- src/core/domain/ports/outbound/user.repository.interface.ts
+++
@@ -1,8 +0,0 @@
-import { UserDto } from 'src/infraestructure/api-rest/dtos/user.dto';
-import { User } from '../../models/user';
-
-export interface UserRepository {
-  createUser(user: User): Promise<UserDto>;
-}
-
-export const UserRepository = Symbol('UserRepository');
