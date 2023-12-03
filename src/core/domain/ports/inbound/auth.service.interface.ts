cb9f4bca32531df007976b22693ab5a59ee34036
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-03 03:06:58 +0100

    authService Interface updated #3

---
+++ src/core/domain/ports/inbound/auth.service.interface.ts
@@ -0,0 +1,9 @@
+import {
+  CreateUserDto,
+  LoginUserDto,
+} from '../../../../infraestructure/api-rest/dtos/user.dto';
+
+export interface IAuthService {
+  register(createUserDto: CreateUserDto);
+  login(loginUserDto: LoginUserDto);
+}
