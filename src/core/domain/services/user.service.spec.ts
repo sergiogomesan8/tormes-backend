28f9aeb4ce2b4a083ce13fd4d858c9da53bf6407
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-03 02:11:57 +0100

    remove duplicated test for user service #2

--- src/core/domain/services/user.service.spec.ts
+++ src/core/domain/services/user.service.spec.ts
@@ -95,31 +95,5 @@ describe('UserService', () => {
         expect(e).toHaveProperty('message', 'Save error');
       }
     });
-
-    it('should throw an error if save executes but then fails', async () => {
-      const user = { name: 'Test User' };
-      const createUserDto: CreateUserDto = {
-        email: 'test@example.com',
-        name: 'John',
-        lastName: 'Doe',
-        phoneNumber: 1234567890,
-        gender: 1,
-        birthdate: 1234567890,
-        userType: 1,
-      };
-      jest
-        .spyOn(userRepository, 'create')
-        .mockImplementation(() => user as any);
-      jest.spyOn(userRepository, 'save').mockImplementation(() => {
-        throw new Error('Save error after execution');
-      });
-
-      try {
-        await service.create(createUserDto);
-      } catch (e) {
-        expect(e).toBeInstanceOf(Error);
-        expect(e).toHaveProperty('message', 'Save error after execution');
-      }
-    });
   });
 });
