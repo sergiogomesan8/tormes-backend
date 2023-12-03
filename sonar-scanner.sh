6f6468e009f216c1cf299c1d27062457dbc2dede
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-03 01:56:27 +0100

    update sonar scanner script #2

--- sonar-scanner.sh
+++ sonar-scanner.sh
@@ -5,10 +5,28 @@ export $(grep -v '^#' .env | xargs)
 
 # Ejecuta el comando sonar-scanner
 sonarScannerCommand="sonar-scanner \
+  -Dsonar.projectKey=$SONAR_PROJECT_KEY \
   -Dsonar.sources=src \
   -Dsonar.host.url=$SONAR_HOST_URL \
   -Dsonar.login=$SONAR_TOKEN"
 
+echo sonarScannerCommand
+$sonarScannerCommand
+
+if [ $? -eq 0 ]
+then
+  echo "SonarQube analysis completed"
+else
+  echo "Error during SonarQube analysis" >&2
+fi
+
+sonarScannerCommand="sonar-scanner.bat \
+  -Dsonar.projectKey=$SONAR_PROJECT_KEY \
+  -Dsonar.sources=src \
+  -Dsonar.host.url=$SONAR_HOST_URL \
+  -Dsonar.login=$SONAR_TOKEN"
+
+echo sonarScannerCommand: $sonarScannerCommand
 $sonarScannerCommand
 
 if [ $? -eq 0 ]
