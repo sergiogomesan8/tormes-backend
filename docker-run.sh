461c3cdc4cf971c57d05aed7ff268479a7e09014
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-10 00:34:50 +0100

    update docker run #14

--- docker-run.sh
+++ docker-run.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 echo "¿Qué contenedor quieres iniciar?"
-echo "1. Sonarqube"
-echo "2. Postgres-database"
-echo "3. Pgadmin"
+echo "1. Postgres-database"
+echo "2. Pgadmin"
+echo "3. Sonarqube"
 read -p "Introduce el número del contenedor que quieres iniciar: " containerNumber
 
 case $containerNumber in
   1)
-    docker-compose up sonarqube
-    ;;
-  2)
+    sudo rm -rf ./docker-data/postgres_data/*
+    echo "Directorio ./docker-data/postgres_data/ borrado."
     docker-compose up postgres-database
     ;;
-  3)
+  2)
     docker-compose up pgadmin
+    ;;
+  3)
+    docker-compose up sonarqube
     ;;
   *)
     echo "Opción no válida."
