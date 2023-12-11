3d6f0855053dd29715de05e9cc3eca54a6caa3a6
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-11 23:13:24 +0100

    update product dto #16

--- src/infraestructure/api-rest/dtos/product.dto.ts
+++ src/infraestructure/api-rest/dtos/product.dto.ts
@@ -1,6 +1,56 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
 
+export class ProductDto {
+  @ApiProperty({
+    description: 'Product id',
+    example: 'product-1',
+  })
+  @IsString()
+  @IsNotEmpty()
+  id: string;
+
+  @ApiProperty({
+    description: 'Product name',
+    example: 'Example Product',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
+    description: 'Product description',
+    example: 'This is an example product.',
+  })
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @ApiProperty({
+    description: 'Product image',
+    example: 'https://example.com/image.jpg',
+  })
+  @IsString()
+  @IsNotEmpty()
+  image: string;
+
+  @ApiProperty({
+    description: 'Product price',
+    example: '100',
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  price: number;
+
+  @ApiProperty({
+    description: 'Product section',
+    example: 'Example Section',
+  })
+  @IsString()
+  @IsNotEmpty()
+  section: string;
+}
+
 export class CreateProductDto {
   @ApiProperty({
     description: 'Product name',
@@ -41,4 +91,18 @@ export class CreateProductDto {
   @IsString()
   @IsNotEmpty()
   section: string;
+
+  constructor(
+    name: string,
+    description: string,
+    image: string,
+    price: number,
+    section: string,
+  ) {
+    this.name = name;
+    this.description = description;
+    this.image = image;
+    this.price = price;
+    this.section = section;
+  }
 }
