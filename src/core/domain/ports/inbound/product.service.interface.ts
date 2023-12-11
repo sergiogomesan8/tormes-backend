634120dbf5512d1c62950f93750c0a9822213589
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-11 23:08:46 +0100

    update product service interface #16

--- src/core/domain/ports/inbound/product.service.interface.ts
+++ src/core/domain/ports/inbound/product.service.interface.ts
@@ -4,7 +4,7 @@ import { Product } from '../../models/product.model';
 export interface IProductService {
   findAll(): Promise<Product[]>;
   findOneById(id: string): Promise<Product | null>;
-  create(createProductDto: CreateProductDto): Promise<Product | null>;
+  create(createProductDto: CreateProductDto): Product;
   //update(id: string, updateProductDto: UpdateProductDto): Promise<Product | null>;
   delete(id: string): Promise<Product | null>;
 }
