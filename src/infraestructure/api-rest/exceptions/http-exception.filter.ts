dd5ce711afc1d905749b5a39231e837b7eec0976
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-16 19:59:42 +0100

    create http exception filter #16

---
+++ src/infraestructure/api-rest/exceptions/http-exception.filter.ts
@@ -0,0 +1,37 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const exceptionResponse = exception.getResponse();
+    let message: string | object;
+
+    if (typeof exceptionResponse === 'object' && exceptionResponse !== null) {
+      message = (exceptionResponse as any).message;
+    } else {
+      message = exceptionResponse;
+    }
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    response.status(status).json({
+      statusCode: status,
+      message: message,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
+  }
+}
