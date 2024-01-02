18192284f0cbb3ad2e92a147afb369b78a9d5af2
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-02 13:42:54 +0100

    add uploadFile pipe builder #20

---
+++ src/infraestructure/api-rest/models/file-interceptor.model.ts
@@ -0,0 +1,8 @@
+export enum FileInterceptorSavePath {
+  PRODUCTS = './tormes/assets/images/products',
+  SECTIONS = './tormes/assets/images/sections',
+}
+
+export const MAX_SIZE_VALIDATOR = 1024 * 1024 * 2;
+
+export const FILE_TYPE_VALIDATOR = /image\/(jpeg|jpg|png)/;
