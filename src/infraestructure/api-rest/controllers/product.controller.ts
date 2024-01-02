e9a64dd040e685c7dbc115ba3a1f217d8de8563c
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-02 13:42:09 +0100

    add FileInterceptor #20

--- src/infraestructure/api-rest/controllers/product.controller.ts
+++ src/infraestructure/api-rest/controllers/product.controller.ts
@@ -3,12 +3,16 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Param,
+  ParseFilePipeBuilder,
   ParseUUIDPipe,
   Patch,
   Post,
+  UploadedFile,
   UseFilters,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import {
   ApiTags,
@@ -21,12 +25,24 @@ import {
   ApiInternalServerErrorResponse,
   ApiParam,
   ApiOperation,
+  ApiBody,
+  ApiConsumes,
 } from '@nestjs/swagger';
-import { ProductService } from '../../../core/domain/services/product.service';
-import { JwtAuthGuard } from '../../../core/domain/services/jwt-config/jwt-auth.guard';
-import { Product } from '../../../core/domain/models/product.model';
-import { CreateProductDto, UpdateProductDto } from '../dtos/product.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { HttpExceptionFilter } from '../exceptions/http-exception.filter';
+import { CreateProductDto, UpdateProductDto } from '../dtos/product.dto';
+import { OptionalFilePipe } from '../pipe-builders/uploadFile.pipe.builder';
+import {
+  FILE_TYPE_VALIDATOR,
+  FileInterceptorSavePath,
+  MAX_SIZE_VALIDATOR,
+} from '../models/file-interceptor.model';
+import { ProductService } from '../../../core/domain/services/product.service';
+import { Product } from '../../../core/domain/models/product.model';
+import { JwtAuthGuard } from '../../../core/domain/services/jwt-config/jwt-auth.guard';
+import * as fs from 'fs';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @ApiTags('product')
 @ApiBearerAuth()
@@ -63,33 +79,115 @@ export class ProductController {
   async findProductById(
     @Param('id', new ParseUUIDPipe()) id: string,
   ): Promise<Product> {
-    return await this.productService.findProductById(id);
+    const product = await this.productService.findProductById(id);
+    return product;
   }
 
   @ApiOperation({
     summary: 'Create a product',
     description: 'Endpoint to create a product',
   })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'Product data and image file',
+    type: CreateProductDto,
+  })
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: FileInterceptorSavePath.PRODUCTS,
+        filename: (req, file, cb) => {
+          const mime = file.mimetype;
+
+          if (!FILE_TYPE_VALIDATOR.test(mime)) {
+            return cb(null, '');
+          }
+          const randomName = Array(32)
+            .fill(null)
+            .map(() => Math.round(Math.random() * 16).toString(16))
+            .join('');
+          const fileName = `${randomName}${extname(file.originalname)}`;
+          return cb(null, fileName);
+        },
+      }),
+    }),
+  )
   @UseGuards(JwtAuthGuard)
   @Post()
   async createProduct(
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: FILE_TYPE_VALIDATOR,
+        })
+        .addMaxSizeValidator({ maxSize: MAX_SIZE_VALIDATOR })
+        .build({ errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY }),
+    )
+    file: Express.Multer.File,
     @Body() createProductDto: CreateProductDto,
   ): Promise<Product> {
-    return this.productService.createProduct(createProductDto);
+    return this.productService.createProduct({
+      ...createProductDto,
+      image: file.filename,
+    });
   }
 
   @ApiOperation({
     summary: 'Update a product by ID',
     description: 'Endpoint to update a product by ID',
   })
+  @ApiConsumes('multipart/form-data')
   @ApiParam({ name: 'id', type: String, description: 'The ID of the product' })
+  @ApiBody({
+    description: 'Product data and image file',
+    type: UpdateProductDto,
+  })
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: FileInterceptorSavePath.PRODUCTS,
+        filename: (req, file, cb) => {
+          if (file) {
+            const mime = file.mimetype;
+
+            if (!FILE_TYPE_VALIDATOR.test(mime)) {
+              return cb(null, '');
+            }
+            const randomName = Array(32)
+              .fill(null)
+              .map(() => Math.round(Math.random() * 16).toString(16))
+              .join('');
+            const fileName = `${randomName}${file.filename}`;
+            return cb(null, fileName);
+          } else {
+            return cb(null, '');
+          }
+        },
+      }),
+    }),
+  )
   @UseGuards(JwtAuthGuard)
   @Patch('/:id')
   async updateProduct(
+    @UploadedFile(new OptionalFilePipe()) file: Express.Multer.File | null,
     @Param('id') id: string,
     @Body() updateProductDto: UpdateProductDto,
   ): Promise<Product> {
-    return await this.productService.updateProduct(id, updateProductDto);
+    if (file.filename !== '') {
+      const existingProduct = await this.productService.findProductById(id);
+      const existingImage = existingProduct.image;
+      const imagePath = `${FileInterceptorSavePath.PRODUCTS}/${existingImage}`;
+      if (fs.existsSync(imagePath)) {
+        fs.unlinkSync(imagePath);
+      }
+
+      return await this.productService.updateProduct(id, {
+        ...updateProductDto,
+        image: file.filename,
+      });
+    } else {
+      return await this.productService.updateProduct(id, updateProductDto);
+    }
   }
 
   @ApiOperation({
@@ -100,6 +198,14 @@ export class ProductController {
   @UseGuards(JwtAuthGuard)
   @Delete('/:id')
   async deleteProduct(@Param('id') id: string) {
+    const existingProduct = await this.productService.findProductById(id);
+    const existingImage = existingProduct.image;
+    if (existingImage) {
+      const imagePath = `${FileInterceptorSavePath.PRODUCTS}/${existingImage}`;
+      if (fs.existsSync(imagePath)) {
+        fs.unlinkSync(imagePath);
+      }
+    }
     return await this.productService.deleteProduct(id);
   }
 }
