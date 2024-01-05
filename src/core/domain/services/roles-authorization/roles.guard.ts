e370d59485038f906fbd22a936fa491783cfe261
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-06 00:45:04 +0100

    create roles guard #24

---
+++ src/core/domain/services/roles-authorization/roles.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { USER_TYPES_KEY } from './roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  matchRoles(roles: number[], userType: number): boolean {
+    return roles.includes(userType);
+  }
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.get(
+      USER_TYPES_KEY,
+      context.getHandler(),
+    );
+    if (!requiredRoles) {
+      console.log('Roles are not defined.', requiredRoles);
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (!user) {
+      console.log('User is undefined. Headers:', request.headers);
+    }
+
+    return requiredRoles.includes(user.userType);
+  }
+}
