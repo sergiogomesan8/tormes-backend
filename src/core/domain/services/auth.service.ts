9177a1bfb8741891a0969f35510401925f1a1888
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-09 16:55:45 +0100

    refresh token service method #24

--- src/core/domain/services/auth.service.ts
+++ src/core/domain/services/auth.service.ts
@@ -5,12 +5,13 @@ import {
 } from '@nestjs/common';
 import { IAuthService } from '../ports/inbound/auth.service.interface';
 import { JwtPayload } from './jwt-config/jwt-playload.interface';
-import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from '../../../infraestructure/api-rest/dtos/user.dto';
 import { UserService } from './user.service';
-import { AuthModel } from '../models/auth.model';
+import { AuthModel, Tokens } from '../models/auth.model';
 import { LoginUserDto } from '../../../infraestructure/api-rest/dtos/auth.dto';
 import * as bcrypt from 'bcrypt';
+import { AccessJwtService } from './jwt-config/access-token/access-jwt.service';
+import { RefreshJwtService } from './jwt-config/refresh-token/refresh-jwt.service';
 
 @Injectable()
 export class AuthService implements IAuthService {
@@ -18,7 +19,8 @@ export class AuthService implements IAuthService {
 
   constructor(
     private readonly userService: UserService,
-    private readonly jwtService: JwtService,
+    private readonly accessJwtService: AccessJwtService,
+    private readonly refreshJwtService: RefreshJwtService,
   ) {}
 
   async register(createUserDto: CreateUserDto): Promise<AuthModel> {
@@ -30,11 +32,17 @@ export class AuthService implements IAuthService {
         password: hashedPassword,
       });
 
-      const token = this.getJwtToken({ email: user.email });
+      const tokens: Tokens = await this.getJwtTokens({
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        userType: user.userType,
+      });
 
       return {
         user_info: user,
-        token,
+        access_token: tokens.access_token,
+        refresh_token: tokens.refresh_token,
       };
     } catch (error) {
       throw new InternalServerErrorException('Error creating user');
@@ -51,16 +59,55 @@ export class AuthService implements IAuthService {
       if (!bcrypt.compareSync(password, user.password))
         throw new UnauthorizedException('Credential are not valid.');
 
+      const tokens: Tokens = await this.getJwtTokens({
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        userType: user.userType,
+      });
+
       return {
         user_info: user,
-        token: this.getJwtToken({ email: email }),
+        access_token: tokens.access_token,
+        refresh_token: tokens.refresh_token,
       };
     } catch (error) {
       throw new InternalServerErrorException('Error logging user');
     }
   }
 
-  private getJwtToken(payload: JwtPayload) {
-    return this.jwtService.sign(payload);
+  async refreshToken(email: string): Promise<AuthModel> {
+    try {
+      const user = await this.userService.findUserByEmail(email);
+
+      if (!user) throw new UnauthorizedException('Credential are not valid.');
+
+      const tokens: Tokens = await this.getJwtTokens({
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        userType: user.userType,
+      });
+
+      return {
+        user_info: user,
+        access_token: tokens.access_token,
+        refresh_token: tokens.refresh_token,
+      };
+    } catch (error) {
+      throw new InternalServerErrorException('Error refreshing token');
+    }
+  }
+
+  private async getJwtTokens(payload: JwtPayload): Promise<Tokens> {
+    const [access_token, refresh_token] = await Promise.all([
+      this.accessJwtService.getJwtAccessToken(payload),
+      this.refreshJwtService.getJwtRefreshToken(payload),
+    ]);
+
+    return {
+      access_token: access_token,
+      refresh_token: refresh_token,
+    };
   }
 }
