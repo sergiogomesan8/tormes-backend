617160c04691ddb7e4f69098a0b4414bd07a7dff
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-09 16:52:29 +0100

    refresh jwt service #24

---
+++ src/core/domain/services/jwt-config/refresh-token/refresh-jwt.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Tokens } from '../../../models/auth.model';
+import { ConfigService } from '@nestjs/config';
+import { JwtPayload } from '../jwt-playload.interface';
+import { IRefreshJwtService } from 'src/core/domain/ports/inbound/refresh-jwt.service.interface';
+
+@Injectable()
+export class RefreshJwtService implements IRefreshJwtService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  getJwtRefreshToken(payload: JwtPayload): Promise<Tokens['refresh_token']> {
+    return this.jwtService.signAsync(
+      {
+        payload,
+      },
+      {
+        secret: this.configService.get('JWT_SECRET_REFRESH_TOKEN'),
+      },
+    );
+  }
+}
