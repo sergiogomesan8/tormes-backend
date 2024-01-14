debf16a78369332664d69488044e948f44fc35a1
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-14 22:54:23 +0100

    create order entity #19

---
+++ src/infraestructure/postgres/entities/order.entity.ts
@@ -0,0 +1,43 @@
+import {
+  OrderStatus,
+  OrderedProduct,
+} from '../../../core/domain/models/order.model';
+import { User } from '../../../core/domain/models/user.model';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'orders' })
+export class OrderEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({ unique: true })
+  orderId: string;
+
+  @Column({ nullable: false })
+  status: OrderStatus;
+
+  @CreateDateColumn({ nullable: false })
+  date: number;
+  @Column({ nullable: false })
+  total: number;
+  @Column({ nullable: true })
+  deliveryAddress?: string;
+  @Column({ nullable: true })
+  paymentMethod?: string;
+  @Column({ nullable: true })
+  trackingNumber?: string;
+
+  @Column({ nullable: false })
+  customer: User['id'];
+  @Column({ nullable: true })
+  customerName?: string;
+  @Column({ nullable: true })
+  customerContact?: string;
+
+  @Column({ nullable: false })
+  orderedProducts: OrderedProduct[];
+}
