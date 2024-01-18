162eec1244aafc7f08a9eed59eba0fc7479b1e76
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-18 21:55:31 +0100

    create UpdateOrderStatus Dto #19

--- src/infraestructure/api-rest/dtos/order.dto.ts
+++ src/infraestructure/api-rest/dtos/order.dto.ts
@@ -1,6 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { OrderedProduct } from '../../../core/domain/models/order.model';
 import {
+  OrderStatus,
+  OrderedProduct,
+} from '../../../core/domain/models/order.model';
+import {
+  IsEnum,
   IsNotEmpty,
   IsNumber,
   IsString,
@@ -100,4 +104,10 @@ export class CreateOrderDto {
     this.paymentMethod = paymentMethod;
     this.orderedProducts = orderedProducts;
   }
+}
+
+export class UpdateOrderStatusDto {
+  @IsEnum(OrderStatus)
+  @IsNotEmpty()
+  status: OrderStatus;
 }
