d25bc3eae215e49a6625bfd0f7fc35119e3a0151
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-18 01:23:53 +0100

    update order service interface #19

--- src/core/domain/ports/inbound/order.service.interface.ts
+++ src/core/domain/ports/inbound/order.service.interface.ts
@@ -1,11 +1,17 @@
-import { CreateOrderDto } from '../../../../infraestructure/api-rest/dtos/order.dto';
-import { Order, OrderStatus } from '../../models/order.model';
+import {
+  CreateOrderDto,
+  UpdateOrderStatusDto,
+} from '../../../../infraestructure/api-rest/dtos/order.dto';
+import { Order } from '../../models/order.model';
 
 export interface IOrderService {
   findAllOrders(): Promise<Order[]>;
   findOrderById(id: string): Promise<Order | null>;
   findAllOrdersByUser(userId: string): Promise<Order[]>;
   createOrder(userId: string, createOrderDto: CreateOrderDto): Promise<Order>;
-  updateOrderStatus(id: string, status: OrderStatus): Promise<Order | null>;
+  updateOrderStatus(
+    id: string,
+    updateOrderStatusDto: UpdateOrderStatusDto,
+  ): Promise<Order | null>;
   deleteOrder(id: string);
 }
