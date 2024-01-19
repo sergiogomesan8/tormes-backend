0d95af5f7ca8c428b2b9b076ea0a1e669868cd9f
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-19 22:26:20 +0100

    create cloudinary service tests and update service #32

--- src/infraestructure/postgres/adapters/cloudinary-config/cloudinary.service.ts
+++ src/infraestructure/postgres/adapters/cloudinary-config/cloudinary.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
 import streamifier from 'streamifier';
 
 @Injectable()
 export class CloudinaryService {
-  constructor(private readonly configService: ConfigService) {}
   async uploadImage(
     file: Express.Multer.File,
   ): Promise<UploadApiResponse | UploadApiErrorResponse> {
     return new Promise((resolve, reject) => {
       const upload = v2.uploader.upload_stream((error, result) => {
-        if (error) return reject(error);
-        resolve(result);
+        if (error) {
+          reject(new Error(error.message));
+        } else {
+          resolve(result);
+        }
       });
 
       streamifier.createReadStream(file.buffer).pipe(upload);
@@ -22,8 +23,11 @@ export class CloudinaryService {
   async deleteImage(publicId: string): Promise<void> {
     return new Promise((resolve, reject) => {
       v2.uploader.destroy(publicId, (error, result) => {
-        if (error) return reject(error);
-        resolve(result);
+        if (error) {
+          reject(new Error(error.message));
+        } else {
+          resolve(result);
+        }
       });
     });
   }
