94e245dadc704312a6e8f6c79e06265f76aff2e4
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-19 22:26:33 +0100

    add CloudinaryModule to ProductModule #32

--- src/core/application/product.module.ts
+++ src/core/application/product.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductController } from '../../infraestructure/api-rest/controllers/product.controller';
 import { ProductService } from '../domain/services/product.service';
 import { ProductEntity } from '../../infraestructure/postgres/entities/product.entity';
+import { CloudinaryModule } from '../../infraestructure/postgres/adapters/cloudinary-config/cloudinary.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductEntity])],
+  imports: [TypeOrmModule.forFeature([ProductEntity]), CloudinaryModule],
   controllers: [ProductController],
   providers: [ProductService],
   exports: [ProductService],
