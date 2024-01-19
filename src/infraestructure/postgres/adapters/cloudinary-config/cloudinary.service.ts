ed4bb78a486560132f6c65727c0d8f5f98edf1b1
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-19 20:18:04 +0100

    update cloudinary service #32

--- src/infraestructure/postgres/adapters/cloudinary-config/cloudinary.service.ts
+++ src/infraestructure/postgres/adapters/cloudinary-config/cloudinary.service.ts
@@ -18,4 +18,13 @@ export class CloudinaryService {
       streamifier.createReadStream(file.buffer).pipe(upload);
     });
   }
+
+  async deleteImage(publicId: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      v2.uploader.destroy(publicId, (error, result) => {
+        if (error) return reject(error);
+        resolve(result);
+      });
+    });
+  }
 }
