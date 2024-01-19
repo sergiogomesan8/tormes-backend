81936c7efeaaec13ef6e62a7673bc7d18359e330
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-19 19:58:47 +0100

    create cloudinary module and service #32

---
+++ src/infraestructure/postgres/adapters/cloudinary-config/cloudinary.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
+import streamifier from 'streamifier';
+
+@Injectable()
+export class CloudinaryService {
+  constructor(private readonly configService: ConfigService) {}
+  async uploadImage(
+    file: Express.Multer.File,
+  ): Promise<UploadApiResponse | UploadApiErrorResponse> {
+    return new Promise((resolve, reject) => {
+      const upload = v2.uploader.upload_stream((error, result) => {
+        if (error) return reject(error);
+        resolve(result);
+      });
+
+      streamifier.createReadStream(file.buffer).pipe(upload);
+    });
+  }
+}
