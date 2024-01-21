c37ee7ae9d7604f37f36dce7525f09d449626816
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-21 14:00:15 +0100

    update cash register module with user module import #41

--- src/core/application/cash-register.module.ts
+++ src/core/application/cash-register.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CashRegisterController } from '../../infraestructure/api-rest/controllers/cash-register.controller';
 import { CashRegisterEntity } from '../../infraestructure/postgres/entities/cash-register.entity';
 import { CashRegisterService } from '../domain/services/cash-register.service';
+import { UserModule } from './user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CashRegisterEntity])],
+  imports: [TypeOrmModule.forFeature([CashRegisterEntity]), UserModule],
   controllers: [CashRegisterController],
   providers: [CashRegisterService],
 })
