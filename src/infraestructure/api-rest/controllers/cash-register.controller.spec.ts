720903893abcfc7d7316d9ca064b9cbb498bd70f
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-21 15:31:58 +0100

    findAllCashRegisters, findCashRegisterById, updateCashRegister, deleteCashRegister and tests #41

--- src/infraestructure/api-rest/controllers/cash-register.controller.spec.ts
+++ src/infraestructure/api-rest/controllers/cash-register.controller.spec.ts
@@ -6,9 +6,15 @@ import {
   CashRegister,
   SeralizedCashRegister,
 } from '../../../core/domain/models/cash-register.model';
-import { CreateCashRegisterDto } from '../dtos/cash-register.dto';
+import {
+  CreateCashRegisterDto,
+  UpdateCashRegisterDto,
+} from '../dtos/cash-register.dto';
 import { Request } from 'express';
-import { InternalServerErrorException } from '@nestjs/common';
+import {
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 
 describe('CashRegisterController', () => {
   let cashRegisterController: CashRegisterController;
@@ -21,7 +27,11 @@ describe('CashRegisterController', () => {
         {
           provide: CashRegisterService,
           useValue: {
+            findAllCashRegisters: jest.fn(),
+            findCashRegisterById: jest.fn(),
             createCashRegister: jest.fn(),
+            updateCashRegister: jest.fn(),
+            deleteCashRegister: jest.fn(),
           },
         },
       ],
@@ -81,6 +91,78 @@ describe('CashRegisterController', () => {
     1000,
   );
 
+  const updateCashRegisterDto = new UpdateCashRegisterDto(
+    coins,
+    bills,
+    200,
+    60,
+    400,
+  );
+
+  describe('findAllCashRegisters', () => {
+    it('should return an array of cash registers', async () => {
+      jest
+        .spyOn(cashRegisterService, 'findAllCashRegisters')
+        .mockResolvedValue([cashRegister]);
+      expect(await cashRegisterController.findAllCashRegisters()).toEqual([
+        expectedResponse,
+      ]);
+      expect(cashRegisterService.findAllCashRegisters).toHaveBeenCalled();
+    });
+
+    it('should return an empty array if no cash registers are found', async () => {
+      jest
+        .spyOn(cashRegisterService, 'findAllCashRegisters')
+        .mockResolvedValue([]);
+      expect(await cashRegisterController.findAllCashRegisters()).toEqual([]);
+      expect(cashRegisterService.findAllCashRegisters).toHaveBeenCalled();
+    });
+
+    it('should return an Http Exception error when it happens', () => {
+      jest
+        .spyOn(cashRegisterService, 'findAllCashRegisters')
+        .mockRejectedValue(new InternalServerErrorException());
+
+      return expect(
+        cashRegisterController.findAllCashRegisters(),
+      ).rejects.toThrow(InternalServerErrorException);
+    });
+  });
+
+  describe('findCashRegisterById', () => {
+    it('should return a cash register by id', async () => {
+      jest
+        .spyOn(cashRegisterService, 'findCashRegisterById')
+        .mockResolvedValue(cashRegister);
+      expect(
+        await cashRegisterController.findCashRegisterById(expect.any(String)),
+      ).toEqual(expectedResponse);
+      expect(cashRegisterService.findCashRegisterById).toHaveBeenCalledWith(
+        expect.any(String),
+      );
+    });
+
+    it('should return a NotFoundException if cash register was not found', () => {
+      jest
+        .spyOn(cashRegisterService, 'findCashRegisterById')
+        .mockRejectedValue(new NotFoundException());
+
+      return expect(
+        cashRegisterController.findCashRegisterById(expect.any(String)),
+      ).rejects.toThrow(NotFoundException);
+    });
+
+    it('should return an Http Exception error when it happens', () => {
+      jest
+        .spyOn(cashRegisterService, 'findCashRegisterById')
+        .mockRejectedValue(new InternalServerErrorException());
+
+      return expect(
+        cashRegisterController.findCashRegisterById(expect.any(String)),
+      ).rejects.toThrow(InternalServerErrorException);
+    });
+  });
+
   describe('createCashRegister', () => {
     it('should create a cash register', async () => {
       const req: Partial<Request> = { user: user };
@@ -111,6 +193,71 @@ describe('CashRegisterController', () => {
           req as Request,
           createCashRegisterDto,
         ),
+      ).rejects.toThrow(InternalServerErrorException);
+    });
+  });
+
+  describe('updateCashRegister', () => {
+    it('should update a cash register', async () => {
+      jest
+        .spyOn(cashRegisterService, 'updateCashRegister')
+        .mockResolvedValue(cashRegister);
+
+      expect(
+        await cashRegisterController.updateCashRegister(
+          expect.any(String),
+          updateCashRegisterDto,
+        ),
+      ).toEqual(cashRegister);
+      expect(cashRegisterService.updateCashRegister).toHaveBeenCalledWith(
+        expect.any(String),
+        updateCashRegisterDto,
+      );
+    });
+
+    it('should return an Http Exception error when it happens', () => {
+      jest
+        .spyOn(cashRegisterService, 'updateCashRegister')
+        .mockRejectedValue(new InternalServerErrorException());
+
+      return expect(
+        cashRegisterController.updateCashRegister(
+          expect.any(String),
+          updateCashRegisterDto,
+        ),
+      ).rejects.toThrow(InternalServerErrorException);
+    });
+  });
+
+  describe('deleteCashRegister', () => {
+    it('should delete a cash register', async () => {
+      jest
+        .spyOn(cashRegisterService, 'findCashRegisterById')
+        .mockResolvedValue(cashRegister);
+      jest.spyOn(cashRegisterService, 'deleteCashRegister').mockResolvedValue({
+        message: `Cash register with id ${expect.any(String)} was deleted.`,
+      });
+
+      expect(
+        await cashRegisterController.deleteCashRegister(expect.any(String)),
+      ).toEqual({
+        message: `Cash register with id ${expect.any(String)} was deleted.`,
+      });
+      expect(cashRegisterService.deleteCashRegister).toHaveBeenCalledWith(
+        expect.any(String),
+      );
+    });
+
+    it('should return an Http Exception error when it happens', () => {
+      jest
+        .spyOn(cashRegisterService, 'findCashRegisterById')
+        .mockResolvedValue(cashRegister);
+      jest
+        .spyOn(cashRegisterService, 'deleteCashRegister')
+        .mockRejectedValue(new InternalServerErrorException());
+
+      return expect(
+        cashRegisterController.deleteCashRegister(expect.any(String)),
       ).rejects.toThrow(InternalServerErrorException);
     });
   });
--- src/infraestructure/api-rest/controllers/cash-register.controller.ts
+++ src/infraestructure/api-rest/controllers/cash-register.controller.ts
@@ -5,6 +5,13 @@ import {
   Post,
   UseFilters,
   UseGuards,
+  Get,
+  ClassSerializerInterceptor,
+  UseInterceptors,
+  ParseUUIDPipe,
+  Param,
+  Patch,
+  Delete,
 } from '@nestjs/common';
 import {
   ApiTags,
@@ -16,11 +23,15 @@ import {
   ApiNotFoundResponse,
   ApiInternalServerErrorResponse,
   ApiOperation,
+  ApiParam,
 } from '@nestjs/swagger';
 import { HttpExceptionFilter } from '../exceptions/http-exception.filter';
 import { CashRegisterService } from '../../../core/domain/services/cash-register.service';
 import { JwtAuthGuard } from '../../../core/domain/services/jwt-config/access-token/access-jwt-auth.guard';
-import { CreateCashRegisterDto } from '../dtos/cash-register.dto';
+import {
+  CreateCashRegisterDto,
+  UpdateCashRegisterDto,
+} from '../dtos/cash-register.dto';
 import {
   CashRegister,
   SeralizedCashRegister,
@@ -48,6 +59,44 @@ export class CashRegisterController {
   constructor(private readonly cashRegisterService: CashRegisterService) {}
 
   @ApiOperation({
+    summary: 'Retrieve all cash registers',
+    description: 'Endpoint to get a list of all cash registers',
+  })
+  @UserTypes(UserType.manager)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Get('/list')
+  async findAllCashRegisters(): Promise<SeralizedCashRegister[]> {
+    const cashRegisters = await this.cashRegisterService.findAllCashRegisters();
+    return cashRegisters.map(
+      (cashRegister) => new SeralizedCashRegister(cashRegister),
+    );
+  }
+
+  @ApiOperation({
+    summary: 'Retrieve a cash register by ID',
+    description: 'Endpoint to get a cash register by ID',
+  })
+  @ApiParam({
+    name: 'id',
+    type: String,
+    description: 'The ID of the cash register',
+  })
+  @UserTypes(UserType.manager)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('/:id')
+  async findCashRegisterById(
+    @Param('id', new ParseUUIDPipe()) id: string,
+  ): Promise<SeralizedCashRegister> {
+    const cashRegister =
+      await this.cashRegisterService.findCashRegisterById(id);
+    if (cashRegister) {
+      const seralizedCashRegister = new SeralizedCashRegister(cashRegister);
+      return seralizedCashRegister;
+    }
+  }
+
+  @ApiOperation({
     summary: 'To close Cash Register',
     description: 'Endpoint to close Cash Register',
   })
@@ -69,5 +118,45 @@ export class CashRegisterController {
       const seralizedCashRegister = new SeralizedCashRegister(cashRegister);
       return seralizedCashRegister;
     }
+  }
+
+  @ApiOperation({
+    summary: 'Update a cash register by ID',
+    description: 'Endpoint to update a cash register by ID',
+  })
+  @ApiParam({
+    name: 'id',
+    type: String,
+    description: 'The ID of the cash register',
+  })
+  @UseGuards(JwtAuthGuard)
+  @Patch('/:id')
+  async updateCashRegister(
+    @Param('id') id: string,
+    @Body() updateCashRegisterDto: UpdateCashRegisterDto,
+  ): Promise<SeralizedCashRegister> {
+    const cashRegister = await this.cashRegisterService.updateCashRegister(
+      id,
+      updateCashRegisterDto,
+    );
+    if (cashRegister) {
+      const seralizedCashRegister = new SeralizedCashRegister(cashRegister);
+      return seralizedCashRegister;
+    }
+  }
+
+  @ApiOperation({
+    summary: 'Delete a cash register by ID',
+    description: 'Endpoint to delete a cash register by ID',
+  })
+  @ApiParam({
+    name: 'id',
+    type: String,
+    description: 'The ID of the cash register',
+  })
+  @UseGuards(JwtAuthGuard)
+  @Delete('/:id')
+  async deleteCashRegister(@Param('id') id: string) {
+    return await this.cashRegisterService.deleteCashRegister(id);
   }
 }
