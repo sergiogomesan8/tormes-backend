314b2c9998d9dd35f3384648236e02e33ac3969d
sergiogomesan8 <102580112+sergiogomesan8@users.noreply.github.com>
2024-09-28 18:44:54 +0200

    create stripe module #53

---
+++ src/infraestructure/stripe/stripe.module.ts
@@ -0,0 +1,24 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { StripeService } from './stripe.service';
+
+@Module({})
+export class StripeModule {
+
+  static forRootAsync(): DynamicModule {
+    return {
+      module: StripeModule,
+      imports: [ConfigModule.forRoot()],
+      providers: [
+        StripeService,
+        {
+          provide: 'STRIPE_API_KEY',
+          useFactory: async (configService: ConfigService) =>
+            configService.get('STRIPE_API_KEY'),
+          inject: [ConfigService],
+        },
+      ],
+      exports: [StripeService],
+    };
+  }
+}
