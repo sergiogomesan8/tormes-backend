dfc59c9ee2ca86104670e2e609c8e8aa1de630c8
sergiogomesan8 <102580112+sergiogomesan8@users.noreply.github.com>
2024-09-28 18:56:40 +0200

    create checkout dto #53

---
+++ src/infraestructure/api-rest/dtos/checkout.dto.ts
@@ -0,0 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+    ShoppingOrderedProduct,
+  } from '../../../core/domain/models/order.model';
+import {
+  IsNotEmpty,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { OrderedProductDto } from './order.dto';
+
+
+export class CheckoutDto {
+    @ApiProperty({
+      description: 'Ordered Products',
+      example: [
+        { productId: 'productId1', amount: 2 },
+        { productId: 'productId2', amount: 3 },
+      ],
+      type: [OrderedProductDto],
+    })
+    @IsNotEmpty()
+    @ValidateNested({ each: true })
+    @Type(() => OrderedProductDto)
+    orderedProducts: ShoppingOrderedProduct[];
+
+    constructor(
+      orderedProducts: ShoppingOrderedProduct[],
+    ) {
+      this.orderedProducts = orderedProducts;
+    }
+}
