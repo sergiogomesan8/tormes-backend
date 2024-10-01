45952adc52efbd8525ea6279d2225530db91b2ca
sergiogomesan8 <102580112+sergiogomesan8@users.noreply.github.com>
2024-10-02 01:51:44 +0200

    update product service interface #56

--- src/core/domain/ports/inbound/product.service.interface.ts
+++ src/core/domain/ports/inbound/product.service.interface.ts
@@ -7,10 +7,11 @@ import { Product } from '../../models/product.model';
 export interface IProductService {
   findAllProducts(): Promise<Product[]>;
   findProductById(id: string): Promise<Product | null>;
-  createProduct(createProductDto: CreateProductDto): Promise<Product>;
+  createProduct(createProductDto: CreateProductDto, file: Express.Multer.File): Promise<Product>;
   updateProduct(
     id: string,
     updateProductDto: UpdateProductDto,
+    file: Express.Multer.File | null
   ): Promise<Product | null>;
   deleteProduct(id: string);
 }
