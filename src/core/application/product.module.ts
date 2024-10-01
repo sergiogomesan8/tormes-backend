e9ef16aa71481e7c759db211cb80c5ddc25e07ab
sergiogomesan8 <102580112+sergiogomesan8@users.noreply.github.com>
2024-10-02 01:53:31 +0200

    update product module with new dependencies #56

--- src/core/application/product.module.ts
+++ src/core/application/product.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductController } from '../../infraestructure/api-rest/controllers/product.controller';
 import { ProductService } from '../domain/services/product.service';
 import { ProductEntity } from '../../infraestructure/postgres/entities/product.entity';
+import { StripeModule } from '../../infraestructure/stripe/stripe.module';
 import { CloudinaryModule } from '../../infraestructure/cloudinary-config/cloudinary.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductEntity]), CloudinaryModule],
+  imports: [TypeOrmModule.forFeature([ProductEntity]), CloudinaryModule, StripeModule.forRootAsync()],
   controllers: [ProductController],
   providers: [ProductService],
   exports: [ProductService],
