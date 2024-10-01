ba44c2da43f22c4a7c1d34af3500b0aa83ea6b49
sergiogomesan8 <102580112+sergiogomesan8@users.noreply.github.com>
2024-10-02 01:51:12 +0200

    update ProductService making transactional functions and adding cloudinary service and stripe service on that transactional functions #56

--- src/core/domain/services/product.service.ts
+++ src/core/domain/services/product.service.ts
@@ -11,13 +11,21 @@ import {
 import { Product } from '../models/product.model';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ProductEntity } from '../../../infraestructure/postgres/entities/product.entity';
-import { QueryFailedError, Repository } from 'typeorm';
+import { DataSource, QueryFailedError, Repository } from 'typeorm';
+import { StripeService } from '../../../infraestructure/stripe/stripe.service';
+import { CloudinaryService } from '../../../infraestructure/cloudinary-config/cloudinary.service';
+import * as fs from 'fs';
+import { FileInterceptorSavePath } from '../../../infraestructure/api-rest/models/file-interceptor.model';
+import Stripe from 'stripe';
 
 @Injectable()
 export class ProductService implements IProductService {
   constructor(
     @InjectRepository(ProductEntity)
     private productRepository: Repository<ProductEntity>,
+    private stripeService: StripeService,
+    private readonly cloudinaryService: CloudinaryService,
+    private dataSource: DataSource,
   ) {}
 
   async findAllProducts(): Promise<Product[]> {
@@ -35,44 +43,117 @@ export class ProductService implements IProductService {
     return product;
   }
 
-  async createProduct(createProductDto: CreateProductDto): Promise<Product> {
-    try {
-      const product = this.productRepository.create(createProductDto);
-      await this.productRepository.save(product);
-      return product;
-    } catch (error) {
-      if (error instanceof QueryFailedError) {
-        throw new ConflictException('Product with this name already exists');
+  async createProduct(createProductDto: CreateProductDto, file: Express.Multer.File): Promise<Product> {
+    return await this.dataSource.transaction(async (transactionalEntityManager) => {
+      try {
+        const image = await this.uploadImage(file);
+        const stripeProduct = await this.createStripeProduct(createProductDto.name, createProductDto.description, createProductDto.price, image);
+
+        const product = this.productRepository.create({
+          ...createProductDto,
+          image,
+          paymentId: stripeProduct.id,
+        });
+
+        await transactionalEntityManager.save(product);
+        return product;
+      } catch (error) {
+        if (error instanceof QueryFailedError) {
+          throw new ConflictException('Product with this name already exists');
+        }
+        throw error;
       }
-    }
+    });
   }
 
   async updateProduct(
     id: string,
     updateProductDto: UpdateProductDto,
+    file: Express.Multer.File | null,
   ): Promise<Product> {
-    const updateResult = await this.productRepository.update(
-      id,
-      updateProductDto,
-    );
-    if (updateResult.affected === 0) {
-      throw new NotFoundException('Product not found');
-    }
-    const updatedProduct = await this.productRepository.findOne({
-      where: { id: id },
+    return await this.productRepository.manager.transaction(async (transactionalEntityManager) => {
+      const existingProduct = await transactionalEntityManager.findOne(ProductEntity, { where: { id } });
+      if (!existingProduct) {
+        throw new NotFoundException('Product not found');
+      }
+
+      let image = existingProduct.image;
+      if (file) {
+        await this.deleteImage(existingProduct.image);
+        image = await this.uploadImage(file);
+      }
+
+      const updatedProductDto = { ...existingProduct, ...updateProductDto, image };
+      const updateResult = await transactionalEntityManager.update(ProductEntity, id, updatedProductDto);
+      if (updateResult.affected === 0) {
+        throw new NotFoundException('Product not found');
+      }
+
+      const updatedProduct = await transactionalEntityManager.findOne(ProductEntity, { where: { id } });
+      if (!updatedProduct) {
+        throw new NotFoundException('Error retrieving updated product');
+      }
+
+      if (existingProduct.paymentId) {
+        await this.updateStripeProduct(existingProduct.paymentId, updatedProduct.name, updatedProduct.description, updatedProduct.price, updatedProduct.image);
+      }
+
+      return updatedProduct as Product;
     });
-    if (!updatedProduct) {
-      throw new NotFoundException('Error retrieving updated product');
-    }
-    return updatedProduct;
   }
 
   async deleteProduct(id: string) {
-    const product = await this.productRepository.findOne({ where: { id: id } });
-    if (!product) {
-      throw new NotFoundException('Product not found');
+    return await this.productRepository.manager.transaction(async (transactionalEntityManager) => {
+      const product = await transactionalEntityManager.findOne(ProductEntity, { where: { id } });
+      if (!product) {
+        throw new NotFoundException('Product not found');
+      }
+
+      if (product.paymentId) {
+        await this.deleteStripeProduct(product.paymentId);
+      }
+
+      const existingImage = product.image;
+      if (existingImage) {
+        await this.deleteImage(existingImage);
+      }
+
+      await transactionalEntityManager.delete(ProductEntity, id);
+      return { message: `Product with id ${id} was deleted.` };
+    });
+  }
+
+  private async createStripeProduct(name: string, description:string, price:number, imageUrl:string): Promise<Stripe.Product> {
+    const unitAmount = price * 100;
+    return await this.stripeService.createProduct(name, description, imageUrl, unitAmount);
+  }
+
+  private async deleteStripeProduct(stripeProductId: string): Promise<void> {
+    await this.stripeService.deleteProduct(stripeProductId);
+  }
+
+  private async updateStripeProduct(stripeProductId: string, name: string, description: string, price: number, imageUrl: string): Promise<Stripe.Product> {
+    const unitAmount = price * 100;
+    return await this.stripeService.updateProduct(stripeProductId, name, description, imageUrl, unitAmount);
+  }
+
+  private async uploadImage(file: Express.Multer.File): Promise<string> {
+    if (process.env.NODE_ENV === 'production') {
+      const result = await this.cloudinaryService.uploadImage(file);
+      return result.url as string;
+    } else {
+      return file.filename;
     }
-    await this.productRepository.delete(id);
-    return { message: `Product with id ${id} was deleted.` };
+  }
+
+  private async deleteImage(image: string): Promise<void> {
+    if (process.env.NODE_ENV === 'production') {
+      await this.cloudinaryService.deleteImage(image);
+    } else {
+      const imagePath = `${FileInterceptorSavePath.PRODUCTS}/${image}`;
+      if (fs.existsSync(imagePath)) {
+        fs.unlinkSync(imagePath);
+      }
+    }
   }
 }
