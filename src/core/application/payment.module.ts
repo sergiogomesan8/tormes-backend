2c2e2141a11b168c273f6ea17475f7a3dcdff98c
sergiogomesan8 <102580112+sergiogomesan8@users.noreply.github.com>
2024-10-07 22:33:30 +0200

    create Payment Module with Stripe Module and Payment Service #56

---
+++ src/core/application/payment.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { PaymentService } from '../domain/services/payment.service';
+import { StripeModule } from '../../infraestructure/stripe/stripe.module';
+
+@Module({
+    imports: [StripeModule.forRootAsync()],
+    providers: [
+        {
+            provide: 'IPaymentService',
+            useClass: PaymentService
+        }
+    ],
+    exports: ['IPaymentService'],
+})
+export class PaymentModule {}
