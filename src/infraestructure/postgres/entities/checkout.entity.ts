da4b75e6e4bf107a6ef6827cf75fd457d75e6d6d
sergiogomesan8 <102580112+sergiogomesan8@users.noreply.github.com>
2024-12-01 21:36:45 +0100

    create CheckoutEntity #64

--- src/infraestructure/postgres/entities/checkout.entity.ts
+++ src/infraestructure/postgres/entities/checkout.entity.ts
@@ -2,11 +2,16 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
   OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { OrderEntity } from './order.entity';
 import { CheckoutStatus } from 'src/core/domain/models/checkout.model';
+import {
+  OrderedProduct,
+  ShoppingOrderedProduct,
+} from 'src/core/domain/models/order.model';
 
 @Entity({ name: 'checkouts' })
 export class CheckoutEntity {
@@ -25,8 +30,25 @@ export class CheckoutEntity {
   @Column({ nullable: false })
   status: CheckoutStatus;
 
+  @Column({ nullable: false })
+  userId: string;
+  @Column({ nullable: true })
+  customerName?: string;
+  @Column({ nullable: true })
+  customerContact?: number;
+  @Column({ nullable: true })
+  deliveryAddress?: string;
+  @Column({ nullable: true })
+  billingAddress?: string;
+  @Column({ nullable: true })
+  paymentMethod?: string;
+  @Column('json', { nullable: true })
+  orderedProducts: ShoppingOrderedProduct[];
+
   @Column('json', { nullable: true })
   billingDetail: any;
   @Column({ unique: true })
   sessionId: string;
+  @Column('json', { nullable: true })
+  session: any;
 }
