4570384c0c62871c9a4da21f46d281387a2a4c93
sergiogomesan8 <102580112+sergiogomesan8@users.noreply.github.com>
2024-12-01 21:38:09 +0100

    remove PaymentController and create CheckoutController #64

--- src/infraestructure/api-rest/controllers/checkout.controller.ts
+++ src/infraestructure/api-rest/controllers/checkout.controller.ts
@@ -1,10 +1,12 @@
 import {
   ApiBadRequestResponse,
   ApiBearerAuth,
+  ApiCreatedResponse,
   ApiForbiddenResponse,
   ApiInternalServerErrorResponse,
   ApiNoContentResponse,
   ApiNotFoundResponse,
+  ApiOperation,
   ApiTags,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
@@ -20,11 +22,16 @@ import {
   Body,
   Headers,
   Logger,
+  UseGuards,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
-import { PaymentService } from 'src/core/domain/services/payment.service';
-import { StripeService } from 'src/infraestructure/stripe/stripe.service';
+import { JwtAuthGuard } from 'src/core/domain/services/jwt-config/access-token/access-jwt-auth.guard';
+import { Request } from 'express';
+import { CreateOrderDto } from '../dtos/order.dto';
+import { CheckoutService } from 'src/core/domain/services/checkout.service';
 
-@ApiTags('payment')
+@ApiTags('checkout')
 @ApiBearerAuth()
 @ApiNoContentResponse({ description: 'No content.' })
 @ApiBadRequestResponse({ description: 'Bad request. Invalid data provided.' })
@@ -37,14 +44,33 @@ import { StripeService } from 'src/infraestructure/stripe/stripe.service';
 })
 @ApiInternalServerErrorResponse({ description: 'Internet Server Error.' })
 @UseFilters(new HttpExceptionFilter())
-@Controller('payment')
-export class PaymentController {
-  private readonly logger = new Logger(PaymentController.name);
+@Controller('checkout')
+export class CheckoutController {
+  private readonly logger = new Logger(CheckoutController.name);
 
-  constructor(
-    private readonly paymentService: PaymentService,
-    private readonly stripeService: StripeService,
-  ) {}
+  constructor(private readonly checkoutService: CheckoutService) {}
+
+  @ApiOperation({
+    summary: 'Create an order, creating first a checkout',
+    description: 'Endpoint to create an order, creating first a checkout',
+  })
+  @ApiCreatedResponse({
+    description: 'Checkout created successfully',
+  })
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Post()
+  async createCheckout(
+    @Req() req: Request,
+    @Body() createOrderDto: CreateOrderDto,
+  ): Promise<any> {
+    const user = req.user;
+    const sessionUrl = await this.checkoutService.createCheckout(
+      user['id'],
+      createOrderDto,
+    );
+    return { sessionUrl: sessionUrl };
+  }
 
   @Post('/webhook')
   async handleStripeWebhook(
@@ -52,19 +78,17 @@ export class PaymentController {
     @Headers('stripe-signature') signature: string,
   ): Promise<void> {
     const payload = (req as any).rawBody;
-    this.logger.verbose("payload", payload)
-    const event = await this.stripeService.verifyWebhookSignature(
+    const event = await this.checkoutService.verifyWebhookSignature(
       payload,
       signature,
     );
-
     if (event) {
       switch (event.type) {
         case 'checkout.session.completed':
-          await this.paymentService.successPayment(event.data.object.id);
+          await this.checkoutService.successCheckout(event.data.object.id);
           break;
         case 'checkout.session.async_payment_failed':
-          await this.paymentService.failedPayment(event.data.object.id);
+          await this.checkoutService.failedCheckout(event.data.object.id);
           break;
         default:
           console.log(`Unhandled event type ${event.type}`);
